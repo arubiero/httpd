7c5add63d6397a83cae251d795e3773eb3313dd6
pm <pm>
2017-06-21 19:54:28 +0200

    refactor repcipe

--- test/metadata.rb
+++ test/metadata.rb
@@ -5,3 +5,6 @@ license 'all_rights'
 description 'Installs/Configures httpd'
 long_description 'Installs/Configures httpd'
 version '0.1.0'
+
+issues_url 'https://github.com/arubiero/httpd.git'
+source_url 'https://github.com/arubiero/httpd.git'
